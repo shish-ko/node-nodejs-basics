f6b808f145966afd3ba2859bd3fc358dd70a6276
shish-ko <shishko.dev@gamil.com>
2023-06-16 01:15:09 +0300

    feat: workers

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,23 @@
-const performCalculations = async () => {
-    // Write your code here
-};
+import path from 'path';
+import { Worker } from 'worker_threads';
+import { getCurrentDirName } from '../fs/utils.js';
+import {cpus} from 'os'
 
+const performCalculations = async () => {
+    const coresNumber= cpus().length;
+    const workersRes=[];
+    let workerData=10;
+    for(let i=0; i < coresNumber; i++) {
+        const worker= new Worker(path.join(getCurrentDirName(import.meta.url), 'worker.js'), {workerData: workerData++});
+        worker.on('message', (msg)=>{
+            workersRes.push(msg);
+            if(workersRes.length  === coresNumber) {
+                workersRes.sort((a, b) => a.id-b.id);
+                workersRes.forEach((item) => delete item.id)
+                const res = JSON.stringify(workersRes);
+                process.stdout.write(res + '\n');
+            }  
+        });
+    }
+};
 await performCalculations();
